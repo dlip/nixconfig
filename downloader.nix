821b5b727599e4d1eb1c6449e42246667cbea440
Dane Lipscombe <dane@lipscombe.com.au>
2021-05-07 21:44:01 +1000

    Add transmission

--- downloader.nix
+++ downloader.nix
@@ -9,6 +9,7 @@
       allowedTCPPorts = [
         8989 # sonarr
         7878 # radarr
+        9091 # transmission
       ];
     };
   };
@@ -16,4 +17,11 @@
   services.sonarr = { enable = true; };
   services.radarr = { enable = true; };
   services.bazarr = { enable = true; };
+  services.transmission = {
+    enable = true;
+    settings = {
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1,10.0.0.*";
+    };
+  };
 }
