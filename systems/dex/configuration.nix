10441640660fa0545805811199f96f7a10b4faf9
Dane Lipscombe <dane@lipscombe.com.au>
2021-05-23 13:05:52 +1000

    Add hydra

--- systems/dex/configuration.nix
+++ systems/dex/configuration.nix
@@ -237,6 +237,22 @@ rec {
     dataDir = "/media/backup/restic";
     extraFlags = [ "--no-auth" ];
   };
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://localhost:3000"; # externally visible URL
+    notificationSender = "hydra@localhost"; # e-mail of hydra service
+    # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
+    buildMachinesFiles = [ ];
+    # you will probably also want, otherwise *everything* will be built from scratch
+    useSubstitutes = true;
+    extraConfig =
+      ''
+        <runcommand>
+          job = *:*:*
+          command = cat $HYDRA_JSON >> /tmp/hydra
+        </runcommand>
+      '';
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
