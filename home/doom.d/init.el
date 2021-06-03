207c5e54d91ebb779e4207c648d2b2f7af18dcb8
Dane Lipscombe <dane@lipscombe.com.au>
2021-06-03 22:16:46 +1000

    Add deft

--- home/doom.d/init.el
+++ home/doom.d/init.el
@@ -26,11 +26,11 @@
        ivy               ; a search engine for love and life
 
        :ui
-       ;;deft              ; notational velocity for Emacs
+       deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       (emoji +unicode)  ; 🍣🙂
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
