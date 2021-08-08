d035b8ed0d318b8821762e9b459d2d9d2371362b
Dane Lipscombe <dane@lipscombe.com.au>
2021-08-08 15:52:22 +1000

    Update xmonad config

--- home/files/xmonad.hs
+++ home/files/xmonad.hs
@@ -1,9 +1,13 @@
 import XMonad
+import XMonad.Util.EZConfig (additionalKeysP)
 
+main :: IO ()
 main =
-  xmonad
-    defaultConfig
+  xmonad $
+    def
       { modMask = mod4Mask, -- Use Super instead of Alt
         terminal = "alacritty"
         -- more changes
       }
+      `additionalKeysP` [ ("M-r", spawn "rofi -show run -drun-show-actions")
+                        ]
