89221f532583a284e4b8b80e8343707a69570810
Dane Lipscombe <dane@lipscombe.com.au>
2021-08-08 19:48:00 +1000

    Add show window

--- home/xmonad-config/app/Main.hs
+++ home/xmonad-config/app/Main.hs
@@ -1,13 +1,15 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops (ewmh) -- Needed to export windows to rofi
 import XMonad.Util.EZConfig (additionalKeysP)
 
 main :: IO ()
 main =
   xmonad $
-    def
-      { modMask = mod4Mask, -- Use Super instead of Alt
-        terminal = "alacritty"
-        -- more changes
-      }
-      `additionalKeysP` [ ("M-r", spawn "rofi -show run -drun-show-actions")
+    ewmh
+      def
+        { modMask = mod4Mask, -- Use Super instead of Alt
+          terminal = "alacritty"
+        }
+      `additionalKeysP` [ ("M-r", spawn "rofi -show run -drun-show-actions"),
+                          ("M-p", spawn "rofi -show window")
                         ]
