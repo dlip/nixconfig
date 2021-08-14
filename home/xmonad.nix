af67e5de1b0e5f67a791949ebf2344ed05f906af
Dane Lipscombe <dane@lipscombe.com.au>
2021-08-14 23:14:32 +1000

    Update config

--- home/xmonad.nix
+++ home/xmonad.nix
@@ -24,7 +24,7 @@
     enable = true;
     extraConfig = ''
       Config
-        { font        = "Fira Code"
+        { font        = "xft:Hack Nerd Font Mono:size=13:bold:antialias=true:hinting=true"
         , borderColor = "#d0d0d0"
         , border      = FullB
         , borderWidth = 3
@@ -33,14 +33,14 @@
         , position    = TopSize C 99 30
         , commands    =
             [ Run Cpu ["-t", "cpu: <fc=#4eb4fa><bar> <total>%</fc>"] 10
-            , Run Network "enp3s0" ["-S", "True", "-t", "eth: <fc=#4eb4fa><rx></fc>/<fc=#4eb4fa><tx></fc>"] 10
+            , Run Network "wlp0s20f3" ["-S", "True", "-t", "net: <fc=#4eb4fa><rx></fc>/<fc=#4eb4fa><tx></fc>"] 10
             , Run Memory ["-t","mem: <fc=#4eb4fa><usedbar> <usedratio>%</fc>"] 10
-            , Run Date "date: <fc=#4eb4fa>%a %d %b %Y %H:%M:%S </fc>" "date" 10
+            , Run Date "<fc=#4eb4fa>%a %d %b %Y %H:%M:%S </fc>" "date" 10
             , Run StdinReader
             ]
         , sepChar     = "%"
         , alignSep    = "}{"
-        , template    = "  %StdinReader% | %cpu% | %memory% | %enp3s0%  }{%date%  "
+        , template    = "  %StdinReader% | %cpu% | %memory% | %wlp0s20f3%  }{ %date%  "
         }
     '';
   };
@@ -122,9 +122,13 @@
   };
   services.blueman-applet.enable = true;
   services.pasystray.enable = true;
+
+  services.flameshot.enable = true;
   home.packages = with pkgs; [
     feh
     xmobar
     alttab
+    light
+    scrot
   ];
 }
