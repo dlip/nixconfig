4f3c546c5e1d18c23c6d096462a84bfcbd528ef3
Dane Lipscombe <dane@lipscombe.com.au>
2021-10-06 19:24:28 +1100

    cleanup

--- home/vscode/rescript/rescript-editor-analysis.nix
+++ home/vscode/rescript/rescript-editor-analysis.nix
@@ -1,22 +1,28 @@
-{ stdenv, fetchFromGitHub, bash, ocaml }:
+{ lib, stdenv, fetchFromGitHub, bash, ocaml }:
+
 stdenv.mkDerivation {
-  name = "rescript-editor-analysis";
-  src = fetchFromGitHub {
-      owner = "rescript-lang";
-      repo = "rescript-vscode";
-      rev = "8d0412a72307b220b7f5774e2612760a2d429059";
-      sha256 = "rHQtfuIiEWlSPuZvNpEafsvlXCj2Uv1YRR1IfvKfC2s=";
-    };
+  pname = "rescript-editor-analysis";
+  version = "1.1.3";
+  src = (fetchFromGitHub {
+    owner = "rescript-lang";
+    repo = "rescript-vscode";
+    rev = "8d0412a72307b220b7f5774e2612760a2d429059";
+    sha256 = "rHQtfuIiEWlSPuZvNpEafsvlXCj2Uv1YRR1IfvKfC2s=";
+  }) + "/analysis";
+
   nativeBuildInputs = [ ocaml ];
 
-  buildPhase = ''
-    cd analysis
+  preBuildPhase = ''
     sed -i 's|/bin/bash|${bash}/bin/bash|g' Makefile
-    make
   '';
 
   installPhase = ''
     mkdir -p $out/bin
     cp ./rescript-editor-analysis.exe $out/bin
   '';
+
+  meta = with lib; {
+    license = licenses.mit;
+    maintainers = with maintainers; [ rhoriguchi ];
+  };
 }
