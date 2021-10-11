664bb771e196d7e64216791321f1f5631ad07737
Dane Lipscombe <dane@lipscombe.com.au>
2021-10-11 21:22:35 +1100

    Tmux options

--- home/tmux.nix
+++ home/tmux.nix
@@ -25,12 +25,19 @@
       set -g mouse on
       bind s split-window -h
       bind v split-window -v
-      TMUX_FZF_LAUNCH_KEY="C-f"
+      TMUX_FZF_LAUNCH_KEY="f" # Key Prefix + f
+
+      TMUX_FZF_OPTIONS="-p -w 62% -h 38%"
+      TMUX_FZF_PANE_FORMAT="[#{window_name}] #{pane_current_command}  [#{pane_width}x#{pane_height}] [history #{history_size}/#{history_limit}, #{history_bytes} bytes] #{?pane_active,[active],[inactive]}"
       TMUX_FZF_POPUP=0
       bind-key r source-file ~/.config/tmux/tmux.conf \; display-message "config reloaded"
       set-option -g status-interval 5
       set-option -g automatic-rename on
       set-option -g automatic-rename-format '#{b:pane_current_path}'
+      bind -n M-Left  previous-window
+      bind -n M-Right next-window
+      bind-key -n M-S-Left swap-window -t -1\; select-window -t -1
+      bind-key -n M-S-Right swap-window -t +1\; select-window -t +1
     '';
   };
 }
