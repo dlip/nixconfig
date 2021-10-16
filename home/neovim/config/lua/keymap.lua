d57d284755ae204162154f129445484913f8442f
Dane Lipscombe <dane@lipscombe.com.au>
2021-10-16 15:21:40 +1100

    simplify keymap

--- home/neovim/config/lua/keymap.lua
+++ home/neovim/config/lua/keymap.lua
@@ -1,171 +1,180 @@
 local map = vim.api.nvim_set_keymap
-local default_opts = {noremap = true, silent = true}
 local cmd = vim.cmd 	-- using cmd to execute vim script
 
--- clear search highlighting
-map('n', '<esc>', ':nohl<CR>', default_opts)
+local keymaps = {
+  -- clear search highlighting
+  {'n', '<esc>', ':nohl<CR>'},
 
--- Escape sequence in terminal
-map('t', '<Esc><Esc>', '<C-\\><C-n>', default_opts)
+  -- Escape sequence in terminal
+  {'t', '<Esc><Esc>', '<C-\\><C-n>'},
 
--- move windows using arrows
-map('', '<C-up>', '<C-w>k', default_opts)
-map('', '<C-down>', '<C-w>j', default_opts)
-map('', '<C-left>', '<C-w>h', default_opts)
-map('', '<C-right>', '<C-w>l', default_opts)
-map('', '<C-S-up>', '<C-w>K', default_opts)
-map('', '<C-S-down>', '<C-w>J', default_opts)
-map('', '<C-S-left>', '<C-w>H', default_opts)
-map('', '<C-S-right>', '<C-w>L', default_opts)
--- map('', '<C-S-right>', '<C-w>y<C-w>l<C-w>pp', default_opts)
--- map('', '<C-S-right>', '<C-w>l:e <C-r>#<CR><C-w>h:bp<CR>', default_opts)
-map('t', '<C-up>', '<C-\\><C-n><C-w>k', default_opts)
-map('t', '<C-down>', '<C-\\><C-n><C-w>j', default_opts)
-map('t', '<C-left>', '<C-\\><C-n><C-w>h', default_opts)
-map('t', '<C-right>', '<C-\\><C-n><C-w>l', default_opts)
-map('t', '<C-S-up>', '<C-\\><C-n><C-w>K', default_opts)
-map('t', '<C-S-down>', '<C-\\><C-n><C-w>J', default_opts)
-map('t', '<C-S-left>', '<C-\\><C-n><C-w>H', default_opts)
-map('t', '<C-S-right>', '<C-\\><C-n><C-w>L', default_opts)
+  -- move windows using arrows
+  {'nvo', '<C-up>', '<C-w>k'},
+  {'nvo', '<C-down>', '<C-w>j'},
+  {'nvo', '<C-left>', '<C-w>h'},
+  {'nvo', '<C-right>', '<C-w>l'},
+  {'nvo', '<C-S-up>', '<C-w>K'},
+  {'nvo', '<C-S-down>', '<C-w>J'},
+  {'nvo', '<C-S-left>', '<C-w>H'},
+  {'nvo', '<C-S-right>', '<C-w>L'},
+  -- {'nvo', '<C-S-right>', '<C-w>y<C-w>l<C-w>pp'},
+  -- {'nvo', '<C-S-right>', '<C-w>l:e <C-r>#<CR><C-w>h:bp<CR>'},
+  {'t', '<C-up>', '<C-\\><C-n><C-w>k'},
+  {'t', '<C-down>', '<C-\\><C-n><C-w>j'},
+  {'t', '<C-left>', '<C-\\><C-n><C-w>h'},
+  {'t', '<C-right>', '<C-\\><C-n><C-w>l'},
+  {'t', '<C-S-up>', '<C-\\><C-n><C-w>K'},
+  {'t', '<C-S-down>', '<C-\\><C-n><C-w>J'},
+  {'t', '<C-S-left>', '<C-\\><C-n><C-w>H'},
+  {'t', '<C-S-right>', '<C-\\><C-n><C-w>L'},
 
-----------------------------------------------------------
--- Colemak remapings:
-----------------------------------------------------------
+  -- arrows
+  {'nvo', 'f', 'k'},
+  {'nvo', 's', 'j'},
+  {'nvo', 'r', 'h'},
+  {'nvo', 't', 'l'},
+  {'nvo', 'F', '<C-u>'},
+  {'nvo', 'S', '<C-d>'},
 
--- arrows
-map('', 'f', 'k', default_opts)
-map('', 's', 'j', default_opts)
-map('', 'r', 'h', default_opts)
-map('', 't', 'l', default_opts)
-map('', 'F', '<C-u>', default_opts)
-map('', 'S', '<C-d>', default_opts)
+  -- Word left/right
+  {'nvo', 'w', 'b'},
+  {'nvo', 'p', 'w'},
+  {'nvo', 'W', 'B'},
+  {'nvo', 'P', 'W'},
 
--- Word left/right
-map('', 'w', 'b', default_opts)
-map('', 'p', 'w', default_opts)
-map('', 'W', 'B', default_opts)
-map('', 'P', 'W', default_opts)
+  -- Beginning/end of line
+  {'nvo', 'R', '^'},
+  {'nvo', 'T', '$'},
+  {'v', 'T', 'g_'},
 
--- Beginning/end of line
-map('', 'R', '^', default_opts)
-map('', 'T', '$', default_opts)
-map('v', 'T', 'g_', default_opts)
+  -- Copy/paste
+  {'n', 'c', 'y'},
+  {'x', 'c', 'y'},
+  {'n', 'd', 'p'},
+  {'x', 'd', 'pgvy'},
+  {'n', 'C', 'yg_'},
+  {'x', 'C', 'y'},
+  {'n', 'D', 'P'},
+  {'x', 'D', 'P'},
+  {'n', 'cc', 'yy'},
 
--- Copy/paste
-map('n', 'c', 'y', default_opts)
-map('x', 'c', 'y', default_opts)
-map('n', 'd', 'p', default_opts)
-map('x', 'd', 'pgvy', default_opts) -- paste without yanking
-map('n', 'C', 'yg_', default_opts)
-map('x', 'C', 'y', default_opts)
-map('n', 'D', 'P', default_opts)
-map('x', 'D', 'P', default_opts)
-map('n', 'cc', 'yy', default_opts)
+  -- Undo/redo
+  {'n', 'z', 'u'},
+  {'x', 'z', ':<C-u>undo<CR>'},
+  {'n', 'Z', '<C-r>'},
+  {'x', 'Z', ':<C-u>redo<CR>'},
+  {'n', 'gz', 'U'},
+  {'x', 'gz', 'U<C-u>undo<CR>'},
 
--- Undo/redo
-map('n', 'z', 'u', default_opts)
-map('x', 'z', ':<C-u>undo<CR>', default_opts)
-map('n', 'Z', '<C-r>', default_opts)
-map('x', 'Z', ':<C-u>redo<CR>', default_opts)
-map('n', 'gz', 'U', default_opts)
-map('x', 'gz', 'U<C-u>undo<CR>', default_opts)
+  -- Visual mode
+  {'n', 'h', 'v'},
+  {'n', 'H', 'V'},
 
--- Visual mode
-map('n', 'h', 'v', default_opts)
-map('n', 'H', 'V', default_opts)
+  -- PageUp/PageDown
+  --     nnoremap <silent> <expr> j (winheight(0)-1) . "\<C-u>"
+  --     nnoremap <silent> <expr> h (winheight(0)-1) . "\<C-d>"
+  --     xnoremap <silent> <expr> j (winheight(0)-1) . "\<C-u>"
+  --     xnoremap <silent> <expr> h (winheight(0)-1) . "\<C-d>"
+  --     nnoremap <silent> <expr> <PageUp> (winheight(0)-1) . "\<C-u>"
+  --     nnoremap <silent> <expr> <PageDown> (winheight(0)-1) . "\<C-d>"
+  --     vnoremap <silent> <expr> <PageUp> (winheight(0)-1) . "\<C-u>"
+  --     vnoremap <silent> <expr> <PageDown> (winheight(0)-1) . "\<C-d>"
+  --     vnoremap <silent> <expr> <S-PageUp> (winheight(0)-1) . "\<C-u>"
+  --     vnoremap <silent> <expr> <S-PageDown> (winheight(0)-1) . "\<C-d>"
 
--- PageUp/PageDown
---     nnoremap <silent> <expr> j (winheight(0)-1) . "\<C-u>"
---     nnoremap <silent> <expr> h (winheight(0)-1) . "\<C-d>"
---     xnoremap <silent> <expr> j (winheight(0)-1) . "\<C-u>"
---     xnoremap <silent> <expr> h (winheight(0)-1) . "\<C-d>"
---     nnoremap <silent> <expr> <PageUp> (winheight(0)-1) . "\<C-u>"
---     nnoremap <silent> <expr> <PageDown> (winheight(0)-1) . "\<C-d>"
---     vnoremap <silent> <expr> <PageUp> (winheight(0)-1) . "\<C-u>"
---     vnoremap <silent> <expr> <PageDown> (winheight(0)-1) . "\<C-d>"
---     vnoremap <silent> <expr> <S-PageUp> (winheight(0)-1) . "\<C-u>"
---     vnoremap <silent> <expr> <S-PageDown> (winheight(0)-1) . "\<C-d>"
+  -- Jumplist navigation
+  {'n', '<C-f>', '<C-i>'},
+  {'n', '<C-s>', '<C-o>'},
+  {'v', '<C-f>', '<C-i>'},
+  {'v', '<C-s>', '<C-o>'},
 
--- Jumplist navigation
-map('n', '<C-f>', '<C-i>', default_opts)
-map('n', '<C-s>', '<C-o>', default_opts)
-map('v', '<C-f>', '<C-i>', default_opts)
-map('v', '<C-s>', '<C-o>', default_opts)
+  -- Quicker word change
+  {'n', 'u<space>', 'ciw'},
 
--- Quicker word change
-map('n', 'u<space>', 'ciw', default_opts)
+  -- Folds
+  {'n', 'j', 'z'},
+  {'x', 'j', 'z'},
 
--- Folds
-map('n', 'j', 'z', default_opts)
-map('x', 'j', 'z', default_opts)
+  -- Change
+  {'n', 'u', 'c'},
+  {'x', 'u', 'c'},
+  {'n', 'U', 'C'},
+  {'x', 'U', 'C'},
+  {'n', 'uu', 'cc'},
 
--- Change
-map('n', 'u', 'c', default_opts)
-map('x', 'u', 'c', default_opts)
-map('n', 'U', 'C', default_opts)
-map('x', 'U', 'C', default_opts)
-map('n', 'uu', 'cc', default_opts)
+  -- Replace
+  {'n', 'y', 'r'},
+  {'x', 'y', 'r'},
+  {'n', 'Y', 'R'},
+  {'x', 'Y', 'R'},
 
--- Replace
-map('n', 'y', 'r', default_opts)
-map('x', 'y', 'r', default_opts)
-map('n', 'Y', 'R', default_opts)
-map('x', 'Y', 'R', default_opts)
+  -- Delete
+  {'n', 'x', 'd'},
+  {'v', 'x', 'd'},
+  {'n', 'X', 'D'},
+  {'v', 'X', 'D'},
+  {'n', 'xx', 'dd'},
 
--- Delete
-map('n', 'x', 'd', default_opts)
-map('v', 'x', 'd', default_opts)
-map('n', 'X', 'D', default_opts)
-map('v', 'X', 'D', default_opts)
-map('n', 'xx', 'dd', default_opts)
+  -- 'til
+  {'nvo', 'l', 't'},
+  {'nvo', 'L', 'T'},
 
--- 'til
-map('', 'l', 't', default_opts)
-map('', 'L', 'T', default_opts)
+  -- Macros (replay the macro recorded by qq)
+  {'n', 'Q', '@q'},
 
--- Macros (replay the macro recorded by qq)
-map('n', 'Q', '@q', default_opts)
+  -- Cursor to bottom of screen
+  {'n', '<C-s>', 'L'},
+  {'v', '<C-s>', 'L'},
 
--- Cursor to bottom of screen
-map('n', '<C-s>', 'L', default_opts)
-map('v', '<C-s>', 'L', default_opts)
+  -- Cursor to top of screen
+  {'n', '<C-f>', 'H'},
+  {'v', '<C-f>', 'H'},
 
--- Cursor to top of screen
-map('n', '<C-f>', 'H', default_opts)
-map('v', '<C-f>', 'H', default_opts)
+  -- Go to mark / switch case
+  {'n', 'b', '`'},
+  {'v', 'b', '`'},
+  {'n', 'B', '~'},
+  {'v', 'B', '~'},
 
--- Go to mark / switch case
-map('n', 'b', '`', default_opts)
-map('v', 'b', '`', default_opts)
-map('n', 'B', '~', default_opts)
-map('v', 'B', '~', default_opts)
+  -- Move lines
+  {'n', '<A-f>', ':m .-2<CR>=='},
+  {'n', '<A-s>', ':m .+1<CR>=='},
+  {'i', '<A-f>', '<Esc>:m .-2<CR>==gi'},
+  {'i', '<A-s>', '<Esc>:m .+1<CR>==gi'},
+  {'v', '<A-f>', ':m \'<-2<CR>gv=gv'},
+  {'v', '<A-s>', ':m \'>+1<CR>gv=gv'},
 
--- Move lines
-map('n', '<A-f>', ':m .-2<CR>==', default_opts)
-map('n', '<A-s>', ':m .+1<CR>==', default_opts)
-map('i', '<A-f>', '<Esc>:m .-2<CR>==gi', default_opts)
-map('i', '<A-s>', '<Esc>:m .+1<CR>==gi', default_opts)
-map('v', '<A-f>', ':m \'<-2<CR>gv=gv', default_opts)
-map('v', '<A-s>', ':m \'>+1<CR>gv=gv', default_opts)
-
--- Navigate back/forward
-map('', '<A-r>', '<C-o>', default_opts)
-map('', '<A-t>', '<C-i>', default_opts)
+  -- Navigate back/forward
+  {'nvo', '<A-r>', '<C-o>'},
+  {'nvo', '<A-t>', '<C-i>'},
 
 
--- Enable enter and backspace in normal mode
-map('n', '<CR>', 'o<Space><Backspace><Esc>', default_opts)
+  -- Enable enter and backspace in normal mode
+  {'n', '<CR>', 'o<Space><Backspace><Esc>'},
+  {'n', '<Backspace>', 'X'},
+
+  -- Enable repeating change of selection
+  {'v', 'gu', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>Ncgn'},
+  {'v', 'gU', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>NcgN'},
+
+  -- Paste in insert mode
+  {'i', '<C-v>', '<C-r>"'},
+}
+
+for i = 1, #keymaps do
+  local opts = {noremap = true, silent = true}
+  local keymap = keymaps[i]
+  local modes = keymap[1]
+  for j = 1, #modes do
+    local mode = modes:sub(j,j)
+    map(mode, keymap[2], keymap[3], opts)
+  end
+end
+
+-- Restore enter functionality in quickfix window
 cmd[[
 au FileType qf nmap <buffer> <CR> <CR>
 ]]
-map('n', '<Backspace>', 'X', default_opts)
-
--- Enable repeating change of selection
-map('v', 'gu', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>Ncgn', default_opts)
-map('v', 'gU', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>NcgN', default_opts)
-
--- Paste in insert mode
-map('i', '<C-v>', '<C-r>"', default_opts)
 
 -- Plugins
 cmd[[
