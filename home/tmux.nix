e6bdde81a87eb859d97bfe46b4920d60228bf23d
Dane Lipscombe <dane@lipscombe.com.au>
2021-10-16 23:38:22 +1100

    update

--- home/tmux.nix
+++ home/tmux.nix
@@ -5,7 +5,6 @@
     enable = true;
     clock24 = true;
     plugins = with pkgs.tmuxPlugins; [
-      vim-tmux-navigator
       prefix-highlight
       sensible
       yank
@@ -20,8 +19,8 @@
     extraConfig = ''
       set -g default-terminal "tmux-256color"
       set -ag terminal-overrides ",xterm-256color:RGB"
-      set-option -g prefix C-a
-      bind C-a send-prefix
+      set-option -g prefix C-t
+      bind-key C-t last-window
       set -g mouse on
       bind s split-window -h
       bind v split-window -v
@@ -43,21 +42,21 @@
       # See: https://github.com/christoomey/vim-tmux-navigator
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
           | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-      bind-key -n 'C-r' if-shell "$is_vim" 'send-keys C-r'  'select-pane -L'
-      bind-key -n 'C-s' if-shell "$is_vim" 'send-keys C-s'  'select-pane -D'
-      bind-key -n 'C-f' if-shell "$is_vim" 'send-keys C-f'  'select-pane -U'
-      bind-key -n 'C-t' if-shell "$is_vim" 'send-keys C-t'  'select-pane -R'
+      bind-key -n 'C-Left' if-shell "$is_vim" 'send-keys C-Left'  'select-pane -L'
+      bind-key -n 'C-Down' if-shell "$is_vim" 'send-keys C-Down'  'select-pane -D'
+      bind-key -n 'C-Up' if-shell "$is_vim" 'send-keys C-Up'  'select-pane -U'
+      bind-key -n 'C-Right' if-shell "$is_vim" 'send-keys C-Right'  'select-pane -R'
       tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
       if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
           "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
       if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
           "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
 
-      bind-key -T copy-mode-vi 'C-r' select-pane -L
-      bind-key -T copy-mode-vi 'C-s' select-pane -D
-      bind-key -T copy-mode-vi 'C-f' select-pane -U
-      bind-key -T copy-mode-vi 'C-t' select-pane -R
-      bind-key -T copy-mode-vi 'C-\' select-pane -l
+      bind-key -T copy-mode-vi 'C-Left' select-pane -L
+      bind-key -T copy-mode-vi 'C-Down' select-pane -D
+      bind-key -T copy-mode-vi 'C-Up' select-pane -U
+      bind-key -T copy-mode-vi 'C-Right' select-pane -R
+      bind-key -T copy-mode-vi 'C-\\' select-pane -l
     '';
   };
 }
