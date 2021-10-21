86a6d80bab995ffea6a6c50cd531caea23b23217
Dane Lipscombe <dane@lipscombe.com.au>
2021-10-22 08:42:05 +1100

    Update

--- home/vscode/rescript/rescript-editor-analysis.nix
+++ home/vscode/rescript/rescript-editor-analysis.nix
@@ -4,14 +4,12 @@ stdenv.mkDerivation {
   pname = "rescript-editor-analysis";
   version = "1.1.3";
 
-  src = (fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "rescript-lang";
     repo = "rescript-vscode";
     rev = "8d0412a72307b220b7f5774e2612760a2d429059";
     sha256 = "rHQtfuIiEWlSPuZvNpEafsvlXCj2Uv1YRR1IfvKfC2s=";
-  });
-
-  /* sourceRoot = "analysis"; */
+  };
 
   nativeBuildInputs = [ ocaml ];
 
@@ -21,9 +19,7 @@ stdenv.mkDerivation {
   '';
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp ./rescript-editor-analysis.exe $out/bin
-    #install -D -m 644 -t "$out/bin" "./rescript-editor-analysis.exe"
+    install -D -m0555 rescript-editor-analysis.exe $out/bin/rescript-editor-analysis.exe
   '';
 
   meta = with lib; {
