35d0e183fa4a6610a14e34a179eeaed5187dcded
Dane Lipscombe <dane@lipscombe.com.au>
2021-10-25 18:55:56 +1100

    Update

--- pkgs/solang/default.nix
+++ pkgs/solang/default.nix
@@ -1,24 +1,44 @@
-{ lib, fetchFromGitHub, rustPlatform }:
-
-rustPlatform.buildRustPackage rec {
-  pname = "solang";
-  version = "12.1.1";
-
-  src = fetchFromGitHub {
-    owner = "hyperledger-labs";
-    repo = "solang";
-    rev = "b34320a96230f0eb712bd94a9e2f268218b65161";
-    sha256 = "pWpuk8Wjzqv01/pcK867D59Hr+jlXOvWLY3HT8knrnw=";
+{ stdenv, fetchurl, autoPatchelfHook }:
+stdenv.mkDerivation {
+  name = "kexpand";
+  src = fetchurl {
+    url = "https://github.com/hyperledger-labs/solang/releases/download/v0.1.8/solang-linux";
+    sha256 = "sha256-+qezaj8+s/oYR0VaHntW3RucMth5IfOQ6tcwhBw4qKE=";
   };
-
-  cargoLock = {
-    lockFileContents = builtins.readFile ./Cargo.lock;
-  };
-
-  meta = with lib; {
-    description = "A fast line-oriented regex search tool, similar to ag and ack";
-    homepage = "https://github.com/BurntSushi/ripgrep";
-    license = licenses.unlicense;
-    maintainers = [ maintainers.tailhook ];
-  };
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+  
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/solang
+    # install -m755 -D solang-linux $out/bin/solang
+  '';
 }
+#
+# { lib, fetchFromGitHub, rustPlatform }:
+#
+# rustPlatform.buildRustPackage rec {
+#   pname = "solang";
+#   version = "12.1.1";
+#
+#   src = fetchFromGitHub {
+#     owner = "hyperledger-labs";
+#     repo = "solang";
+#     rev = "b34320a96230f0eb712bd94a9e2f268218b65161";
+#     sha256 = "pWpuk8Wjzqv01/pcK867D59Hr+jlXOvWLY3HT8knrnw=";
+#   };
+#
+#   cargoLock = {
+#     lockFileContents = builtins.readFile ./Cargo.lock;
+#   };
+#
+#   meta = with lib; {
+#     description = "A fast line-oriented regex search tool, similar to ag and ack";
+#     homepage = "https://github.com/BurntSushi/ripgrep";
+#     license = licenses.unlicense;
+#     maintainers = [ maintainers.tailhook ];
+#   };
+# }
+#
