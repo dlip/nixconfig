66dbc797323c3fd8bfd38794ea0780932febbebd
Dane Lipscombe <dane@lipscombe.com.au>
2021-11-29 23:11:48 +1100

    Fix rofi

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,9 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.rofi = {
     enable = true;
     terminal = "${pkgs.alacritty}/bin/alacritty";
-    theme = ./rofi.theme;
+    theme = ./rofi.theme.rasi;
+    font = "FiraCode Nerd Font 10";
+    extraConfig = {
+      show-icons = true;
+      icon-theme = "Papirus";
+      display-drun = "";
+      disable-history = false;
+      sidebar-mode = false;
+    };
   };
 }
