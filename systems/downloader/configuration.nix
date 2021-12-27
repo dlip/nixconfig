6087004a63719985c346f8befc681d873a7c0a35
Dane Lipscombe <dane@lipscombe.com.au>
2021-12-27 15:23:06 +1100

    Add key

--- systems/downloader/configuration.nix
+++ systems/downloader/configuration.nix
@@ -24,7 +24,16 @@ in
     };
   };
 
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  # This will automatically import SSH keys as age keys
+  sops.age.sshKeyPaths = [ "/var/lib/ssh/ssh_host_ed25519_key" ];
+  # This is using an age key that is expected to already be in the filesystem
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  # This will generate a new key if the key specified above does not exist
+  sops.age.generateKey = true;
   # qbittorrent stops downloading when vpn gets reconnected
+  sops.secrets.restic-encryption = { };
+
   systemd.services.restart-qbittorrent = {
     enable = true;
     description = "Restart qbittorrent";
