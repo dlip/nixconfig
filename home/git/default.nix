85a79f13c362a6ccaa676079745648c3aaf940af
Dane Lipscombe <danelipscombe@gmail.com>
2022-03-10 17:09:04 +1100

    Update theme

--- home/git/default.nix
+++ home/git/default.nix
@@ -15,19 +15,8 @@
       };
       interactive = { diffFilter = "delta --color-only --features=interactive"; };
       delta = {
-        features = "decorations";
-        interactive = {
-          keep-plus-minus-markers = false;
-        };
-        decorations = {
-          commit-decoration-style = "blue ol";
-          commit-style = "raw";
-          file-style = "omit";
-          hunk-header-decoration-style = "blue box";
-          hunk-header-file-style = "red";
-          hunk-header-line-number-style = "#067a00";
-          hunk-header-style = "file line-number syntax";
-        };
+        features = "calochortus-lyallii";
+        side-by-side = true;
       };
       credential = { helper = "store"; };
       status = { showUntrackedFiles = "all"; };
