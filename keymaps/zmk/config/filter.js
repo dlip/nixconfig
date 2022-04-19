07293d36445f53292f3853c3ee7ba07c965133de
Dane Lipscombe <danelipscombe@gmail.com>
2022-04-19 18:46:53 +1000

    Use existing combos

--- keymaps/zmk/config/filter.js
+++ keymaps/zmk/config/filter.js
@@ -16,53 +16,78 @@ const readline = require('readline');
       if (word.length < 3) {
         return;
       }
-
-      //first letters strategy
-      const cs = keys.split('');
-      combo = "";
-      index = "";
-      for (let x = 0; x < cs.length; x++) {
-        if (combo.split('').includes(cs[x])) {
-          continue;
-        }
-        combo += cs[x];
-        // need to sort combo to ensure no dupes with different order
-        index = combo.split('').sort().join('');
-
-        if (!used[index]) {
-          break;
-        }
-      }
-
-      // consonants strategy
-      if (combo.length > 2) {
-        ccombo = "";
-        cindex = "";
-        const ccs = keys.replace(/[aeiou]/g, '').split('');
-        for (let x = 0; x < ccs.length; x++) {
-          // ignore letters occuring multiple letters
-          if (ccombo.split('').includes(ccs[x])) {
+      let combo = "";
+      let index = "";
+      if (!keys) {
+        keys = word;
+        //first letters strategy
+        const cs = keys.split('');
+        
+        for (let x = 0; x < cs.length; x++) {
+          if (combo.split('').includes(cs[x])) {
             continue;
           }
-          ccombo += ccs[x];
+          combo += cs[x];
           // need to sort combo to ensure no dupes with different order
-          cindex = ccombo.split('').sort().join('');
+          index = combo.split('').sort().join('');
 
-          if (!used[cindex] && (cindex.length < index.length || used[index])) {
-            index = cindex;
-            combo = ccombo;
+          if (!used[index]) {
             break;
           }
         }
+
+
+        // consonants strategy
+        if (combo.length > 2) {
+          let ccombo = "";
+          const ccs = keys.replace(/[aeiou]/g, '').split('');
+          for (let x = 0; x < ccs.length; x++) {
+            // ignore letters occuring multiple letters
+            if (ccombo.split('').includes(ccs[x])) {
+              continue;
+            }
+            ccombo += ccs[x];
+            // need to sort combo to ensure no dupes with different order
+            let cindex = ccombo.split('').sort().join('');
+
+            if (!used[cindex] && (cindex.length < index.length || used[index])) {
+              index = cindex;
+              combo = ccombo;
+              break;
+            }
+          }
+        }
+
+        // // first and last letter strategy
+        // if (combo.length > 2) {
+        //   let first = keys.charAt(0);
+        //   let last = keys.charAt(keys.length - 1);
+        //   if (first !== last) {
+        //     let ccombo = first + last;
+        //     // need to sort combo to ensure no dupes with different order
+        //     let cindex = ccombo.split('').sort().join('');
+        //
+        //     if (!used[cindex]) {
+        //       index = cindex;
+        //       combo = ccombo;
+        //     }
+        //   }
+        // }
+      } else {
+        index = keys.split('').sort().join('');
+        combo = keys;
       }
+
 
       if (used[index]) {
         // if used try to use the existing keys
-        combo = keys;
-        index = combo.split('').sort().join('');
-        if (used[index]) {
-          throw new Error(`Combo ${combo} already used by ${used[index]}`)
-        }
+        // combo = [...new Set(keys).split('')];
+        // combo = keys;
+        //
+        // index = combo.split('').sort().join('');
+        // if (used[index]) {
+          throw new Error(`Can't use combo '${combo}' for word '${word}' already used by ${used[index]}`)
+        // }
       }
       used[index] = word;
 
