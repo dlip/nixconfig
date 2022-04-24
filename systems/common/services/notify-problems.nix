818e973cca34fba68a71387a7d390506fc9d260a
Dane Lipscombe <danelipscombe@gmail.com>
2022-04-25 00:38:41 +1000

    Use notify for alerts

--- systems/common/services/notify-problems.nix
+++ systems/common/services/notify-problems.nix
@@ -1,43 +1,23 @@
 { config, lib, pkgs, modulesPath, ... }:
-with lib;
-let
-  cfg = config.my.services.notify-problems;
-in
 {
-  options.my.services.notify-problems = {
-    adminEmail = mkOption {
-      type = types.str;
-      default = "dane@lipscombe.com.au";
-      example = "user@example.com";
-      description = ''
-        Admin email address
-      '';
-    };
+  config.sops.secrets.notify = {
+    sopsFile = ../secrets/secrets.yaml;
   };
-
-  config = {
-    systemd.services."notify-problems@" = {
-      enable = true;
-      description = "Alert email for %i to user";
-      serviceConfig = {
-        Type = "oneshot";
-        User = "nobody";
-        Group = "systemd-journal";
-      };
-
-      environment.SERVICE = "%i";
-      script = ''
-        # TODO fix
-        sendmail -t <<ERRMAIL
-        To: ${cfg.adminEmail}
-        From: ${cfg.adminEmail}
-        Subject: $HOSTNAME: $SERVICE Error
-        Content-Transfer-Encoding: 8bit
-        Content-Type: text/plain; charset=UTF-8
-
-        $(systemctl status --full "$SERVICE")
-        ERRMAIL
-      '';
+  config.systemd.services."notify-problems@" = {
+    enable = true;
+    description = "Alert email for %i to user";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      Group = "systemd-journal";
     };
+
+    environment.SERVICE = "%i";
+    script = ''
+      echo "
+      $HOSTNAME: $SERVICE Error
+      $(systemctl status --full "$SERVICE")
+      " | ${pkgs.notify}/bin/notify -provider-config ${config.sops.secrets.notify.path}
+    '';
   };
 }
