0091ad90a20a8d2c441ebad55c9b01c8039feabc
Dane Lipscombe <danelipscombe@gmail.com>
2022-07-09 17:02:27 +1000

    Fix actual

--- overlays/personal/actualServer/default.nix
+++ overlays/personal/actualServer/default.nix
@@ -9,6 +9,13 @@ let
     url = "https://github.com/kelektiv/node.bcrypt.js/releases/download/v${bcrypt_version}/bcrypt_lib-v${bcrypt_version}-napi-v3-${arch}-glibc.tar.gz";
     sha256 = "3R3dBZyPansTuM77Nmm3f7BbTDkDdiT2HQIrti2Ottc=";
   };
+
+  bettersqlite3_version = "7.5.0";
+  bettersqlite3_lib = fetchurl {
+    url = "https://github.com/WiseLibs/better-sqlite3/releases/download/v${bettersqlite3_version}/better-sqlite3-v${bettersqlite3_version}-node-v93-linux-x64.tar.gz";
+    sha256 = "sha256-n9OvLPm2XuzaJjbikPeAr96eCVNukK2Cn0KaKLIIRws=";
+  };
+
 in
 pkgs.mkYarnPackage {
   name = "actual-sync";
@@ -17,14 +24,23 @@ pkgs.mkYarnPackage {
     pushd node_modules/bcrypt
     mkdir -p ./lib/binding && tar -C ./lib/binding -xf ${bcrypt_lib}
     popd
-
-    pushd ./deps/actual-sync
-    yarn build
+    pushd node_modules/better-sqlite3
+    tar xf ${bettersqlite3_lib}
     popd
   '';
+
   postInstall = ''
-    rm $out/libexec/actual-sync/deps/actual-sync/node_modules
-    mv $out/libexec/actual-sync/node_modules $out/libexec/actual-sync/deps/actual-sync/node_modules
+    mv $out/libexec/actual-sync/deps/actual-sync $out/src
+    rm $out/src/node_modules
+    mv $out/libexec/actual-sync/node_modules $out/src/node_modules
+    rm -rf $out/libexec
+
+    cp ${./config.js} $out/src/load-config.js
+    cat > $out/bin/actual-server <<EOF
+      #! ${stdenv.shell} -e
+      ${nodejs}/bin/node $out/src/app.js
+    EOF
+    chmod +x $out/bin/actual-server
   '';
 
   doDist = false;
