cabc8153f0f4946a73a7bdf538fb2f6e5590455a
Dane Lipscombe <danelipscombe@gmail.com>
2022-07-09 17:06:25 +1000

    Add service

--- systems/dex/configuration.nix
+++ systems/dex/configuration.nix
@@ -65,14 +65,25 @@ rec {
 
 
   systemd.services.xboxdrv = {
-     wantedBy = [ "multi-user.target" ]; 
-     after = [ "network.target" ];
-     serviceConfig = {
-       Type = "forking";
-       User = "root";
-       ExecStart = ''${pkgs.xboxdrv}/bin/xboxdrv --daemon --detach --pid-file /var/run/xboxdrv.pid --dbus disabled --silent --deadzone 4000 --deadzone-trigger 10% --mimic-xpad-wireless'';
-     };
-   };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      Type = "forking";
+      User = "root";
+      ExecStart = ''${pkgs.xboxdrv}/bin/xboxdrv --daemon --detach --pid-file /var/run/xboxdrv.pid --dbus disabled --silent --deadzone 4000 --deadzone-trigger 10% --mimic-xpad-wireless'';
+    };
+  };
+
+  systemd.services.actual = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    description = "Actual Server";
+    serviceConfig = {
+      Type = "forking";
+      User = "root";
+      ExecStart = ''${pkgs.actual-server}/bin/actual-server'';
+    };
+  };
 
   networking.nat.enable = true;
   networking.nat.internalInterfaces = [ "wg0" "ve-+" ];
