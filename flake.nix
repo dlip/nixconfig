7fa49935b84a2d5ddd3fe7f25b5acf82838faa43
Dane Lipscombe <danelipscombe@gmail.com>
2022-07-28 16:18:26 +1000

    Add work email

--- flake.nix
+++ flake.nix
@@ -238,6 +238,7 @@
                     useUserPackages = true;
                     users = {
                       dane = {
+                        home.email = "dane.lipscombe@playtravel.com.au";
                         home.xrandrCommand = "xrandr --auto --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1";
                         imports = [
                           ./home
