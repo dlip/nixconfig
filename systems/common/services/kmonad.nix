5d580dc560696a25a00ad790f06129bea8a80c3a
Dane Lipscombe <danelipscombe@gmail.com>
2022-09-18 20:53:07 +1000

    Add charachorder config

--- systems/common/services/kmonad.nix
+++ systems/common/services/kmonad.nix
@@ -9,6 +9,11 @@
           device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
           config = builtins.readFile (../../../keymaps/kmonad/sweep20.kbd);
         };
+        charachorder = {
+          name = "charachorder";
+          device = "/dev/input/by-id/usb-CharaChorder_CharaChorder_1_A21D040450583234352E3120FF070B2A-if02-event-kbd";
+          config = builtins.readFile (../../../keymaps/kmonad/charachorder-silver.kbd);
+        };
       };
   };
 }
