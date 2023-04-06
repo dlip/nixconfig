b3c5f5da7395eea785965748e05d0ce6242af7d0
Dane Lipscombe <danelipscombe@gmail.com>
2023-04-07 09:23:27 +1000

    open new terminal

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -3,5 +3,6 @@
 
   home.file = {
     "${config.xdg.configHome}/zellij/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/zellij/config.kdl";
+    "${config.xdg.configHome}/zellij/layouts".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/zellij/layouts";
   };
 }
