064f49a1e10bbc2c70bb09e7e95bde951e265cef
Dane Lipscombe <danelipscombe@gmail.com>
2023-04-08 17:38:07 +1000

    Add mappings

--- home/nnn/default.nix
+++ home/nnn/default.nix
@@ -15,7 +15,7 @@ let
   );
 
   nnn = pkgs.writeShellScriptBin "nnn" ''
-    export NNN_PLUG='f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview;r:renamer;c:.cbcp'
+    export NNN_PLUG='f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;v:imgview;r:renamer;c:.cbcp;s:!echo $PWD/$nnn|xclip -sel c*'
 
     BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
     export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
