56e8b3abd290244a2a814dd1cc3ccfdecf4ba03e
Dane Lipscombe <danelipscombe@gmail.com>
2023-04-18 12:20:31 +1000

    Add theme

--- home/leftwm/default.nix
+++ home/leftwm/default.nix
@@ -4,4 +4,9 @@
   ...
 }: {
   home.file."${config.xdg.configHome}/leftwm".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/leftwm/config";
+  home.file."${config.xdg.configHome}/eww".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/leftwm/config/themes/basic_eww/eww-bar";
+
+  home.packages = with pkgs.unstable; [
+    eww
+  ];
 }
