81c7a0a75cfa8b0ed4057212ec7875a4ac06c375
Dane Lipscombe <danelipscombe@gmail.com>
2023-04-18 17:14:33 +1000

    Update colors

--- home/leftwm/default.nix
+++ home/leftwm/default.nix
@@ -9,6 +9,7 @@
   home.packages = with pkgs.unstable; [
     eww
     polybar
+    feh
     dmenu
     pamixer
     playerctl
@@ -24,26 +25,26 @@
     tray = "always";
   };
 
-  services.stalonetray = {
-    enable = true;
-    config = {
-      decorations = null;
-      transparent = false;
-      dockapp_mode = null;
-      geometry = "6x1-510+10";
-      max_geometry = "0x0";
-      background = "#313846";
-      kludges = "force_icons_size";
-      grow_gravity = "NE";
-      icon_gravity = "NE";
-      icon_size = 21;
-      slot_size = 30;
-      sticky = true;
-      # window_strut = null;
-      window_type = "dock";
-      window_layer = "top";
-      # no_shrink = false;
-      skip_taskbar = true;
-    };
-  };
+  # services.stalonetray = {
+  #   enable = true;
+  #   config = {
+  #     decorations = null;
+  #     transparent = false;
+  #     dockapp_mode = null;
+  #     geometry = "6x1-510+10";
+  #     max_geometry = "0x0";
+  #     background = "#313846";
+  #     kludges = "force_icons_size";
+  #     grow_gravity = "NE";
+  #     icon_gravity = "NE";
+  #     icon_size = 21;
+  #     slot_size = 30;
+  #     sticky = true;
+  #     # window_strut = null;
+  #     window_type = "dock";
+  #     window_layer = "top";
+  #     # no_shrink = false;
+  #     skip_taskbar = true;
+  #   };
+  # };
 }
