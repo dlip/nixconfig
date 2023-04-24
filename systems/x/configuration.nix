8363ff3f0c37b2435f2fc60d31f7291e683d8e97
Dane Lipscombe <danelipscombe@gmail.com>
2023-04-24 11:16:51 +1000

    Update wireguard

--- systems/x/configuration.nix
+++ systems/x/configuration.nix
@@ -57,39 +57,15 @@ in {
     allowedUDPPorts = [51820]; # Clients and peers can use the same port, see listenport
   };
 
-  # Enable WireGuard
+  # TODO: get this working
   # networking.wireguard.interfaces = {
-  #   # "wg0" is the network interface name. You can name the interface arbitrarily.
   #   wg0 = {
-  #     # Determines the IP address and subnet of the client's end of the tunnel interface.
-  #     ips = [ "10.100.0.4/32" ];
-  #     listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-  #
-  #     # Path to the private key file.
-  #     #
-  #     # Note: The private key can also be included inline via the privateKey option,
-  #     # but this makes the private key world-readable; thus, using privateKeyFile is
-  #     # recommended.
-  #     privateKeyFile = "/home/dane/privatekey";
-  #
+  #     ips = ["10.100.0.5/24"];
+  #     privateKeyFile = config.sops.secrets.wireguard-key.path;
+  #     listenPort = 51820;
+
   #     peers = [
-  #       # For a client configuration, one peer entry for the server will suffice.
-  #
-  #       {
-  #         # Public key of the server (not a file path).
-  #         publicKey = "+YVMX+HXcyFsfxGWdWC+WdI6nUMkyMdtxsohVDJavlQ=";
-  #
-  #         # Forward all the traffic via VPN.
-  #         allowedIPs = [ "10.10.0.0/24" ];
-  #         # Or forward only particular subnets
-  #         #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-  #
-  #         # Set this to the server IP and port.
-  #         endpoint = "lipscombe.ddns.net:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-  #
-  #         # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-  #         persistentKeepalive = 25;
-  #       }
+  #       (import ../common/wireguard/dex-peer.nix)
   #     ];
   #   };
   # };
