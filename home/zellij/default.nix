9880564755471d51ad63e4b543fad3154be83151
Dane Lipscombe <danelipscombe@gmail.com>
2023-06-15 11:46:07 +1000

    update zellij

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }: {
-  home.packages = [ pkgs.unstable.zellij ];
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = [pkgs.unstable.zellij];
 
   home.file = {
-    "${config.xdg.configHome}/zellij/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/zellij/config.kdl";
-    "${config.xdg.configHome}/zellij/layouts".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/zellij/layouts";
+    "${config.xdg.configHome}/zellij".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/zellij/config";
   };
 }
