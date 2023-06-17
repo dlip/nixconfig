af8dc72ed1b25e821ff9272cbe124060338decac
Dane Lipscombe <danelipscombe@gmail.com>
2023-06-17 18:03:32 +1000

    Update g config

--- systems/g/configuration.nix
+++ systems/g/configuration.nix
@@ -1,58 +1,55 @@
-{ config, pkgs, lib, ... }:
-let params = {
-  hostname = "g";
-};
-in
 {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      (import ../common params)
-      # ../common/desktop/awesome.nix
-      ../common/desktop/kde.nix
-      ../common/services/kmonad.nix
-    ];
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  params = {
+    hostname = "g";
+  };
+in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    (import ../common params)
+    # ../common/desktop/awesome.nix
+    ../common/desktop/i3.nix
+    ../common/services/kmonad.nix
+  ];
 
   virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "dane" ];
+  users.extraGroups.vboxusers.members = ["dane"];
 
   users.users.dane = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "networkmanager" "dialout" "adbusers" ]; # Enable ‘sudo’ for the user.
+    extraGroups = ["wheel" "docker" "networkmanager" "dialout" "adbusers"]; # Enable ‘sudo’ for the user.
     shell = "/etc/profiles/per-user/dane/bin/zsh";
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  services.xserver.screenSection = ''
-    Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
-    Option         "AllowIndirectGLXProtocol" "off"
-    Option         "TripleBuffer" "on"
-  '';
-
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.prime = {
-    sync.enable = true;
-    # offload.enable = true;
+    # sync.enable = true;
+    offload.enable = true;
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
 
-
-  sops.secrets.dex-dane = { };
+  sops.secrets.dex-dane = {};
   fileSystems."/mnt/dex-dane" = {
     device = "//10.10.0.123/dane";
     fsType = "cifs";
-    options = [ "credentials=${config.sops.secrets.dex-dane.path},uid=1000,gid=100,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s" ];
+    options = ["credentials=${config.sops.secrets.dex-dane.path},uid=1000,gid=100,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"];
   };
   fileSystems."/mnt/dex-media" = {
     device = "//10.10.0.123/media";
     fsType = "cifs";
-    options = [ "credentials=${config.sops.secrets.dex-dane.path},uid=1000,gid=100,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s" ];
+    options = ["credentials=${config.sops.secrets.dex-dane.path},uid=1000,gid=100,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"];
   };
   fileSystems."/mnt/dex-media2" = {
     device = "//10.10.0.123/media2";
     fsType = "cifs";
-    options = [ "credentials=${config.sops.secrets.dex-dane.path},uid=1000,gid=100,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s" ];
+    options = ["credentials=${config.sops.secrets.dex-dane.path},uid=1000,gid=100,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"];
   };
   # services.k3s = {
   #   enable = false;
@@ -61,7 +58,7 @@ in
   # };
 
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
   services.flatpak.enable = true;
 
   # networking.wireguard.interfaces = {
