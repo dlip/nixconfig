bf213803f3045ec0b927cc07153c2c1393b8d0ab
Dane Lipscombe <danelipscombe@gmail.com>
2023-09-15 19:37:18 +1000

    Add canary

--- systems/common/services/kmonad.nix
+++ systems/common/services/kmonad.nix
@@ -1,11 +1,11 @@
 {...}: {
   services.kmonad = {
-    enable = false;
+    enable = true;
     keyboards = {
       sweep = {
         name = "sweep";
         device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-        config = builtins.readFile ../../../keymaps/kmonad/sweep22.kbd;
+        config = builtins.readFile ../../../keymaps/kmonad/sweep23.kbd;
       };
       # charachorder = {
       #   name = "charachorder";
