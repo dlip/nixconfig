17dd380af552202ceed8ec77fba80f8aa5b8ec28
Dane Lipscombe <danelipscombe@gmail.com>
2024-01-16 16:50:09 +1100

    add path

--- home/session.nix
+++ home/session.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.sessionVariables = {
     GOPATH = "$HOME/go";
-    PATH = "$HOME/bin:$HOME/.local/bin:$GOPATH/bin:$HOME/.cargo/bin:/etc/profiles/per-user/$USER/bin:/opt/homebrew/bin:$PATH";
+    PATH = "$HOME/code/nixconfig/bin:$HOME/bin:$HOME/.local/bin:$GOPATH/bin:$HOME/.cargo/bin:/etc/profiles/per-user/$USER/bin:/opt/homebrew/bin:$PATH";
     EDITOR = "nvim";
     LANG = "en_AU.UTF-8";
     LC_ALL = "en_AU.UTF-8";
