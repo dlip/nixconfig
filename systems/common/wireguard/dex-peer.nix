b8c2ffe1e42b21267fde2c9ef41d5ead22e73a0f
Dane Lipscombe <danelipscombe@gmail.com>
2024-01-31 14:46:49 +1100

    Fix host

--- systems/common/wireguard/dex-peer.nix
+++ systems/common/wireguard/dex-peer.nix
@@ -8,7 +8,7 @@
   #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
   # Set this to the server IP and port.
-  endpoint = "lipscombe.ddns.net:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+  endpoint = "lips-home.duckdns.org:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
 
   # Send keepalives every 25 seconds. Important to keep NAT tables alive.
   persistentKeepalive = 25;
