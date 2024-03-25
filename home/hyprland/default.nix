f415ee74c380a65a9e03d856ef81994aa66de19a
Dane Lipscombe <danelipscombe@gmail.com>
2024-03-25 14:46:31 +1100

    Add waybar

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -3,7 +3,21 @@
   config,
   ...
 }: {
+  imports = [
+    ../waybar
+  ];
+
   home.file = {
     "${config.xdg.configHome}/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nixconfig/home/hyprland/hyprland.conf";
   };
+
+  home.packages = with pkgs; [
+    playerctl
+    brightnessctl
+    sway-contrib.grimshot
+    grimblast
+    cliphist
+    rofi
+    fuzzel
+  ];
 }
